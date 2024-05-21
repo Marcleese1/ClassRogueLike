6e571c7c8ab0cd42500e75bc7281a1b67096ef5c
Marcleese1 <mleese200@caledonian.ac.uk>
2024-05-22 00:14:38 +0100

    finished basic abilities

--- Source/ClassRoguelike/ClassRoguelike.h
+++ Source/ClassRoguelike/ClassRoguelike.h
@@ -9,5 +9,16 @@ enum class BaseAbilityID : uint8
 {
 	None UMETA(DisplayName = "None"),
 	Confirm UMETA(DisplayName = "Confirm"),
-	Cancel UMETA(DisplayName = "Cancel")
+	Cancel UMETA(DisplayName = "Cancel"),
+	Ability1 UMETA(DisplayName = "Ability1")
+};
+
+UENUM(BlueprintType)
+enum class FighterAbilities : uint8
+{
+	Slash UMETA(DisplayName = "Slash"),
+	Charge UMETA(DisplayName = "Charge"),
+	Slam UMETA(DisplayName = "Slam"),
+	Block UMETA(DisplayName = "Block"),
+	Whirlwind UMETA(DisplayName = "Whirlwind")
 };
