413d9cb8502e38d59a962e4fb4987478a0c681c7
Marcleese1 <mleese200@caledonian.ac.uk>
2024-07-07 16:58:15 +0100

    attributes work

--- Source/ClassRoguelike/Private/Character/Abilities/AttributeSets/CharacterAttributeSetBase.cpp
+++ Source/ClassRoguelike/Private/Character/Abilities/AttributeSets/CharacterAttributeSetBase.cpp
@@ -3,7 +3,7 @@
 
 UCharacterAttributeSetBase::UCharacterAttributeSetBase()
 {
-    // Initialize attributes to some default values (these will be overwritten by Gameplay Effects)
+    // Default values (these will be overridden by Gameplay Effects)
     Health.SetCurrentValue(100.0f);
     Health.SetBaseValue(100.0f);
     MaxHealth.SetCurrentValue(100.0f);
@@ -57,7 +57,6 @@ void UCharacterAttributeSetBase::OnRep_MaxMana(const FGameplayAttributeData& Old
     }
 }
 
-
 void UCharacterAttributeSetBase::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, Mana, OldStamina);
@@ -74,7 +73,6 @@ void UCharacterAttributeSetBase::OnRep_MaxStamina(const FGameplayAttributeData& 
     }
 }
 
-// Implement the replication functions for the new attributes
 void UCharacterAttributeSetBase::OnRep_DamageReduction(const FGameplayAttributeData& OldDamageReduction)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, DamageReduction, OldDamageReduction);
@@ -104,35 +102,51 @@ void UCharacterAttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProp
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MaxHealth, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, Mana, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MaxMana, COND_None, REPNOTIFY_Always);
-
-    // Add replication for the new attributes
+    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, Stamina, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MaxStamina, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, DamageReduction, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, CriticalHitDamage, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, CriticalHitChance, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MovementSpeed, COND_None, REPNOTIFY_Always);
 }
-
-
+//UCharacterAttributeSetBase::UCharacterAttributeSetBase()
+//{
+//    // Initialize attributes to some default values (these will be overwritten by Gameplay Effects)
+//    Health.SetCurrentValue(100.0f);
+//    Health.SetBaseValue(100.0f);
+//    MaxHealth.SetCurrentValue(100.0f);
+//    MaxHealth.SetBaseValue(100.0f);
+//
+//    Mana.SetCurrentValue(100.0f);
+//    Mana.SetBaseValue(100.0f);
+//    MaxMana.SetCurrentValue(100.0f);
+//    MaxMana.SetBaseValue(100.0f);
+//
+//    Stamina.SetCurrentValue(100.0f);
+//    Stamina.SetBaseValue(100.0f);
+//    MaxStamina.SetCurrentValue(100.0f);
+//    MaxStamina.SetBaseValue(100.0f);
+//}
+//
 //void UCharacterAttributeSetBase::OnRep_Level(const FGameplayAttributeData& OldLevel)
 //{
 //    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, Level, OldLevel);
-//}
-//
-//void UCharacterAttributeSetBase::OnRep_Health(const FGameplayAttributeData& OldHealth)
-//{
-//    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, Health, OldHealth);
 //}
 //
 //void UCharacterAttributeSetBase::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
 //{
 //    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, MaxHealth, OldMaxHealth);
 //
-//    // Set Health to MaxHealth if Health is currently zero
 //    if (Health.GetCurrentValue() == 0.0f)
 //    {
 //        Health.SetCurrentValue(MaxHealth.GetCurrentValue());
 //        Health.SetBaseValue(MaxHealth.GetBaseValue());
 //    }
+//}
+//
+//void UCharacterAttributeSetBase::OnRep_Health(const FGameplayAttributeData& OldHealth)
+//{
+//    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, Health, OldHealth);
 //}
 //
 //void UCharacterAttributeSetBase::OnRep_Mana(const FGameplayAttributeData& OldMana)
@@ -144,27 +158,31 @@ void UCharacterAttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProp
 //{
 //    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, MaxMana, OldMaxMana);
 //
-//    if( Mana.GetCurrentValue() == 0.0f)
-//	{
-//		Mana.SetCurrentValue(MaxMana.GetCurrentValue());
-//		Mana.SetBaseValue(MaxMana.GetBaseValue());
-//	}
+//    if (Mana.GetCurrentValue() == 0.0f)
+//    {
+//        Mana.SetCurrentValue(MaxMana.GetCurrentValue());
+//        Mana.SetBaseValue(MaxMana.GetBaseValue());
+//    }
 //}
+//
 //
 //void UCharacterAttributeSetBase::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
 //{
-//    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, Stamina, OldStamina);
+//    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, Mana, OldStamina);
 //}
 //
 //void UCharacterAttributeSetBase::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina)
 //{
-//    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, MaxStamina, OldMaxStamina);
-//    if(Stamina.GetCurrentValue() == 0.0f){
-//		Stamina.SetCurrentValue(MaxStamina.GetCurrentValue());
-//		Stamina.SetBaseValue(MaxStamina.GetBaseValue());
-//	}
+//    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, MaxMana, OldMaxStamina);
+//
+//    if (Stamina.GetCurrentValue() == 0.0f)
+//    {
+//        Stamina.SetCurrentValue(MaxStamina.GetCurrentValue());
+//        Stamina.SetBaseValue(MaxStamina.GetBaseValue());
+//    }
 //}
 //
+//// Implement the replication functions for the new attributes
 //void UCharacterAttributeSetBase::OnRep_DamageReduction(const FGameplayAttributeData& OldDamageReduction)
 //{
 //    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSetBase, DamageReduction, OldDamageReduction);
@@ -194,11 +212,11 @@ void UCharacterAttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProp
 //    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MaxHealth, COND_None, REPNOTIFY_Always);
 //    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, Mana, COND_None, REPNOTIFY_Always);
 //    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MaxMana, COND_None, REPNOTIFY_Always);
-//    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, Stamina, COND_None, REPNOTIFY_Always);
-//    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MaxStamina, COND_None, REPNOTIFY_Always);
 //
+//    // Add replication for the new attributes
 //    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, DamageReduction, COND_None, REPNOTIFY_Always);
 //    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, CriticalHitDamage, COND_None, REPNOTIFY_Always);
 //    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, CriticalHitChance, COND_None, REPNOTIFY_Always);
 //    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSetBase, MovementSpeed, COND_None, REPNOTIFY_Always);
 //}
+
